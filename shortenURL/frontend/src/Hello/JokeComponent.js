b0de4418627cb940e9b8ee289b2908f0d2b6735f
Anurag Kumar <anurag.hcu@gmail.com>
2023-07-16 19:59:50 +0530

    refactored JokeComponent.js

--- shortenURL/frontend/src/Hello/JokeComponent.js
+++ shortenURL/frontend/src/Hello/JokeComponent.js
@@ -1,26 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
-const JokeComponent = () => {
+function Joke() {
   const [joke, setJoke] = useState("");
+  const [fetching, setFetching] = useState(false);
+  const intervalId = useRef(null);
 
   const fetchJoke = async () => {
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/random_joke"
-    );
+    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
     const data = await response.json();
     setJoke(`${data.setup} - ${data.punchline}`);
   };
 
+  const handleButtonClick = () => {
+    if (fetching) {
+      clearInterval(intervalId.current);
+    } else {
+      fetchJoke();
+      intervalId.current = setInterval(fetchJoke, 10000);  // Fetch new joke every 10 seconds
+    }
+    setFetching(!fetching);
+  };
+
   useEffect(() => {
-    fetchJoke(); // Fetch a joke on component mount
+    return () => {
+      // Cleanup interval on component unmount
+      if (intervalId.current) {
+        clearInterval(intervalId.current);
+      }
+    };
   }, []);
 
   return (
     <div>
-      <button onClick={fetchJoke}>Get Joke</button>
+      <button onClick={handleButtonClick}>{fetching ? "Stop" : "Get Joke"}</button>
       <textarea readOnly value={joke} style={{width: "80%", height: "20vh"}} />
     </div>
   );
-};
+}
 
-export default JokeComponent;
+export default Joke;
